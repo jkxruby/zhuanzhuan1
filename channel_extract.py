f8f44ed130bf9b6ba453bc5dfe4d0f8bf65a0603
jkxruby <jkxruby@gmail.com>
2017-10-25 16:17:30 +0800

    define the category url list as a function

--- channel_extract.py
+++ channel_extract.py
@@ -3,13 +3,17 @@ from bs4 import BeautifulSoup
 
 
 start_url = 'http://bj.ganji.com/wu/'
-wb_data = requests.get(start_url)
-soup = BeautifulSoup(wb_data.text, 'lxml')
-part_category_links = soup.select('div.main-pop dt a')  # 此变量生成一个字典的集合，无法使用的，当我们通过for in 时，会调出单独的字典，再使用字典函数get()等等
 
-for part_category_link in part_category_links:
-    category_links =  start_url.split('/')[2] + str(part_category_link.get('href'))
-    print(category_links)
+def get_category_urls(start_url):
+    wb_data = requests.get(start_url)
+    soup = BeautifulSoup(wb_data.text, 'lxml')
+    part_category_urls = soup.select('div.main-pop dt a')  # 此变量生成一个字典的集合，无法使用的，当我们通过for in 时，会调出单独的字典，再使用字典函数get()等等
+
+    for part_category_url in part_category_urls:
+        category_url =  start_url.split('/')[2] + str(part_category_url.get('href'))
+        print(category_url)
+
+get_category_urls(start_url)
 
 category_links_list = '''
 
