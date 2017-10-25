d65369cb15c9a6e76acc072cedc7d82dd860e68c
jkxruby <jkxruby@gmail.com>
2017-10-25 16:06:28 +0800

    the first step, finish the category links list

--- channel_extract.py
+++ channel_extract.py
@@ -11,7 +11,37 @@ for part_category_link in part_category_links:
     category_links =  start_url.split('/')[2] + str(part_category_link.get('href'))
     print(category_links)
 
-category_links_lists = '''
+category_links_list = '''
 
+bj.ganji.com/shouji/
+bj.ganji.com/shoujipeijian/
+bj.ganji.com/bijibendiannao/
+bj.ganji.com/taishidiannaozhengji/
+bj.ganji.com/diannaoyingjian/
+bj.ganji.com/wangluoshebei/
+bj.ganji.com/shumaxiangji/
+bj.ganji.com/youxiji/
+bj.ganji.com/xuniwupin/
+bj.ganji.com/jiaju/
+bj.ganji.com/jiadian/
+bj.ganji.com/zixingchemaimai/
+bj.ganji.com/rirongbaihuo/
+bj.ganji.com/yingyouyunfu/
+bj.ganji.com/fushixiaobaxuemao/
+bj.ganji.com/meironghuazhuang/
+bj.ganji.com/yundongqicai/
+bj.ganji.com/yueqi/
+bj.ganji.com/tushu/
+bj.ganji.com/bangongjiaju/
+bj.ganji.com/wujingongju/
+bj.ganji.com/nongyongpin/
+bj.ganji.com/xianzhilipin/
+bj.ganji.com/shoucangpin/
+bj.ganji.com/baojianpin/
+bj.ganji.com/laonianyongpin/
+bj.ganji.com/gou/
+bj.ganji.com/qitaxiaochong/
+bj.ganji.com/xiaofeika/
+bj.ganji.com/menpiao/
 
 '''
