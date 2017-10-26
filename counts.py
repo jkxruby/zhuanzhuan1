839b3ec5c6d6ec50e00420cce8cd84a2b90c562f
jkxruby <jkxruby@gmail.com>
2017-10-26 14:34:09 +0800

    add try.py just for some unexpect error

--- counts.py
+++ counts.py
@@ -3,6 +3,6 @@ from page_parsing import item_url_list
 from page_parsing import item_info
 
 while True:
-    time.sleep(5)
+    time.sleep(1)
     print(item_url_list.find().count())
     print(item_info.find().count())
---
+++ try.py
@@ -0,0 +1,9 @@
+# 略错程序，防止小错误导致程序终止，选择忽略该错误
+
+def try_to_make(ass):
+    try:
+        print(1/ass)
+    except (ZeroDivisionError,TypeError):
+        print('ok~')
+try_to_make('0')
+#  如上，通过 try except 实现忽略错误(重要程序有错必究，不能忽略的)
